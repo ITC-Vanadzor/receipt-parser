0fe535803a02020f387e32ab7940212028a9fdb2
Ani121293 <ani_saghatelyan@mail.ru>
2016-03-15 01:02:54 +0400

    Deleted node_modules folder, some files, which won't be used, added corrected version of statistic page

--- server/modules/user/router.js
+++ server/modules/user/router.js
@@ -2,15 +2,9 @@
 var express = require('express');
 var controller = require('./controller')
 var router = express.Router();
-var acl = require('../../components/acl');
 
 module.exports = function() {
-  // router.get('*', acl.allow('Poghos'));
-  // router.post('*', acl.allow('Poghos'));
   router.get('/', controller.get);
-
-  router.get('/:id', controller.getById);
-
   router.post('/', controller.create);
   router.post('/login', controller.login);
   router.post('/registration', controller.registration);
